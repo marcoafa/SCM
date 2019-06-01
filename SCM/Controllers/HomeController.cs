b7896efcc45e566222ed01edfb40fb960e1bad7e
MarcoFuentes <marco.fuentes.isc@gmail.com>
2019-06-01 07:58:17 -0700

    First Commint starting project

--- SCM/Controllers/HomeController.cs
+++ SCM/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SCM.Models;
 
 namespace SCM.Controllers
 {
@@ -14,30 +12,14 @@ namespace SCM.Controllers
         {
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
+        public IActionResult Home()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Login()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+      
     }
 }
