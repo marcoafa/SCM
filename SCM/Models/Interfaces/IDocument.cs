8fe15173b02b472134b92ffef685a69e807f22d5
MarcoFuentes <marco.fuentes.isc@gmail.com>
2019-07-01 19:51:32 -0700

    Faild

---
+++ SCM/Models/Interfaces/IDocument.cs
@@ -0,0 +1,13 @@
+﻿using SCM.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SCM.Models.Interfaces
+{
+    public interface IDocument : IRepository<Document>
+    {
+
+    }
+}
---
+++ SCM/Models/Repository/DocumentRepository.cs
@@ -0,0 +1,18 @@
+﻿using SCM.Models.Entities;
+using SCM.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SCM.Models.Repository
+{
+    public class DocumentRepository: Repository<Document>, IDocument
+    {
+        public DocumentRepository(RecicladoraContext context) : base(context)
+        {
+
+        }
+
+    }
+}
